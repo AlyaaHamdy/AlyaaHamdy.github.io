9703c092d6c22af33dc260a4ced1db24e482e272
AlyaaHamdy <alyaa.hamdy@outlook.com>
2024-01-17 15:39:14 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*<!-- ================Typing Animation================== -->*/
+ var typed =new Typed(".typing",{
+    strings:["","Web Developer","Front-End Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
